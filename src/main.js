4635a26f1a8c7f9773960f0b97e5dd19d11a055a
INGANZAMARUMPU <jonkur96@gmail.com>
2023-12-21 14:57:08 +0200

    import components globally and built getIcon()

--- src/main.js
+++ src/main.js
@@ -7,15 +7,64 @@ import {
   IonRouterOutlet 
 } from '@ionic/vue';
 import '@ionic/core/css/ionic.bundle.css'
+import * as allIcons from "ionicons/icons";
+import { App as CapacitorApp } from '@capacitor/app';
 
-const app = createApp(App).use(IonicVue).use(router);
+window.axios = axios;
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router)
+  .use(store)
 
-app.component("IonRouterOutlet", IonRouterOutlet)
-app.component("IonApp", IonApp)
-app.component("IonPage", IonPage)
-app.component("IonHeader", IonHeader)
-app.component("IonToolbar", IonToolbar)
-app.component("IonTitle", IonTitle)
-app.component("IonContent", IonContent)
+const components = {
+  IonApp, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonicVue,
+  IonRouterOutlet
+};
 
+Object.entries(components).forEach(([name, component]) => {
+  app.component(name, component)
+})
+
+app.mixin({
+  methods: {
+    getIcon(name) {
+      return allIcons[name];
+    },
+    money(x) {
+      let cash = parseFloat(x).toFixed(0)
+      if(!x) return "0";
+      return cash.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    },
+    logOut(x) {
+      alertController.create({
+        header: 'Attention!',
+        message: 'Voulez-vous vraiment vous deconnecter?',
+        buttons: [
+          {
+            text: 'laisser',
+            role: 'cancel'
+          },
+          {
+            text: 'OUI',
+            handler: () => {
+              localStorage.clear() 
+              this.$store.state.user = null
+            },
+          },
+        ],
+      }).then(res => {
+        res.present();
+      });
+    },
+  },
+  computed:{
+  }
+})
+CapacitorApp.addListener('backButton', ({canGoBack}) => {
+  if((!canGoBack) || (window.history.state.back == null)){
+    CapacitorApp.exitApp();
+  } else {
+    window.open(window.history.state.back, "_self")
+  }
+});
 app.mount('#app');
